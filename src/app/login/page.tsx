e3a5565984f3b57b5b297b62ba12aca8a31e395a
Carlos Jose Sanchez <carlosjosesancheze@gmail.com>
2026-04-17 10:04:14 -0400

    force dinamic

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,8 @@
 import { env } from '@/env';
 import { redirect, notFound } from 'next/navigation';
 
+export const dynamic = 'force-dynamic';
+
 export default function LoginRedirectPage() {
   const defaultAccountSlug = env.IAM_DEFAULT_ACCOUNT_SLUG;
 
